e3ffd285d37ebfd597e55c925f513a60c4beca03
Yolopix <13918281+y0lopix@users.noreply.github.com>
2020-07-27 01:35:16 +0200

    Allow multiple links

--- bypass.py
+++ bypass.py
@@ -4,10 +4,11 @@ from lxml import html
 from lxml import etree
 from sys import argv
 
-if len(argv)==2:
-    tree = html.fromstring(get(argv[1]).text)
-    redir_link = tree.xpath("//*[@id='compteur']/noscript/a")
-    encoded_url=redir_link[0].attrib['href'].split("url=")[1].split("&")[0]
-    print(unquote(encoded_url))
+if len(argv)>1:
+    for i in argv[1:]:
+        tree = html.fromstring(get(i).text)
+        redir_link = tree.xpath("//*[@id='compteur']/noscript/a")
+        encoded_url=redir_link[0].attrib['href'].split("url=")[1].split("&")[0]
+        print(unquote(encoded_url))
 else:
-    print("Wrong number of arguments.")
+    print("No arguments provided.")
